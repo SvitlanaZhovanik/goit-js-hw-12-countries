bddf1c6f0d60599143e4f633837dfc0a907566a9
SvitlanaZhovanik <Lanasvetcat@gmail.com>
2021-10-27 14:17:00 +0300

    сделано задание 12

---
+++ src/fetchCountries.js
@@ -0,0 +1,5 @@
+function fetchCountries(searchQuery) {
+  return fetch(`https://restcountries.com/v2/name/${searchQuery}`).then(response => response.json()) 
+}
+
+export default fetchCountries
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+import countryCardTpl from './templates/country-card.hbs'
+import countrylistTpl from './templates/country-list.hbs'
+import fetchCountries from "./fetchCountries.js"
+import debounce  from 'lodash.debounce';
+import {error} from '@pnotify/core'
+
+const inputRef = document.querySelector("input");
+const cardWrapper = document.querySelector('.wrapper');
+
+inputRef.addEventListener('input', debounce(onInputClick, 500))
+
+function onInputClick(evt) {
+    fetchCountries(evt.target.value).then(countryRender).catch(error => {alert("Enter country name, please")})
+}
+
+function countryRender(countries) {
+    cardWrapper.innerHTML = ' ';
+if (countries.length > 10) {
+    error({
+        text: "Too many matches found. Please enter a more specific query!",  
+       })
+      
+} else if(countries.length > 1 && countries.length <= 10){
+   
+    countries.forEach(countryRenderList)
+    
+} else {
+    countryRenderCard(countries)
+}
+}
+
+function countryRenderList(country) {
+    const markup = countrylistTpl(country);
+    cardWrapper.insertAdjacentHTML('beforeend', markup)     
+}
+
+function countryRenderCard([country]) {
+    const markup = countryCardTpl(country);
+    cardWrapper.innerHTML = markup 
+}
+
